10d86e20f9264cce855674ecddbd9b63d12f8891
Mateusz Choma <kontakt@coderoad.pl>
2020-05-01 14:24:53 +0200

    simplest render function added

--- script.js
+++ script.js
@@ -7,6 +7,27 @@ const addTask = function (newTaskText) {
     }
 
     tasks[tasks.length] = newTask
+
+    render()
+}
+
+const render = function (containerSelector = 'body') {
+    const container = document.querySelector(containerSelector)
+
+    if (!container) return
+
+    container.innerHTML = ''
+
+    for (let i = 0; i < tasks.length; i++) {
+        const div = document.createElement('div')
+
+        div.innerText = tasks[i].text
+
+        container.appendChild(div)
+    }
 }
 
 addTask('Wynieś śmieci')
+addTask('Umyj okna')
+
+render()
